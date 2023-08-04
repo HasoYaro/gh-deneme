45425a9cbce460b16cba562a130ef4c17e808460
HasoYaro <s42combi@hotmail.com>
2023-08-04 11:07:43 +0300

    a

--- src/pages/components/Router.js
+++ src/pages/components/Router.js
@@ -2,16 +2,19 @@ import Home from '../Home'
 import Error from '../Error'
 import Header from './Header'
 import Footer from './Footer'
+import '../styles/Router.css'
 import { HashRouter, Routes, Route } from 'react-router-dom'
 
 export default function Routerr() {
     return (
         <>
-        <HashRouter hashType="hashbang">
+        <HashRouter>
         <Header />
         <Routes>
-            <Route path='/a' exact element={<Home />} />
-            <Route path='*' element={<Error />} />
+            <div className='parent'>
+                <Route path='/a' exact element={<Home />} />
+                <Route path='*' element={<Error />} />
+            </div>
         </Routes>
         <Footer />
         </HashRouter>
