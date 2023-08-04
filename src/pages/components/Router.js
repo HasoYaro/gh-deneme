31268ed0f31815c603e5d58b6519f05226bbb3b4
HasoYaro <s42combi@hotmail.com>
2023-08-04 10:14:14 +0300

    initial commit

--- src/pages/components/Router.js
+++ src/pages/components/Router.js
@@ -1,6 +1,7 @@
 import Header from './Header'
 import Footer from './Footer'
 import Home from '../Home'
+import Error from '../Error'
 import '../styles/Router.css'
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
 
@@ -23,6 +24,7 @@ export default function Routerr() {
         <Routes>
             <Route path="/" element={<Layout />}>
                 <Route path='/' element={<Home />} />
+                <Route path='*' element={<Error />} />
             </Route>
         </Routes>
         </BrowserRouter>
