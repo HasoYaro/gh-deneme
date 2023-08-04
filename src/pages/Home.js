e6297aeb73914a10068bafd9d428efd79ed9297c
HasoYaro <s42combi@hotmail.com>
2023-08-04 12:11:21 +0300

    a

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,37 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './styles/Home.css'
 
 export default function Home() {
 
     const[isim,setIsim] = useState('')
-    const[hata,setHata] = useState(false)
     const[userInput,setUserInput] = useState('')
 
     function selamla(e) {
         e.preventDefault()
 
-        if(userInput === "")
-        {  
-            setHata(true)
-            setIsim('İsim boş bırakılamaz')
-
-        }
-        else 
-        {
-            setIsim('Merhaba, '+userInput)
-            setHata(false)
-        }
-       
+        setIsim('Merhaba, '+userInput)
     }
         return(
             <>
             <h1></h1>
             <div className='home-cont'>
-                <form>
+                <form onSubmit={selamla}>
                 <ul>
                     <li>Selamlamak istediğiniz kişinin ismini girin</li>
-                    <li><input autoComplete='off' value={userInput} onChange={(e) => setUserInput(e.target.value)} required id="home-isim-input" /><button onClick={selamla}>Selamla</button></li>
-                    <li style={{color: hata ? 'red' : '#212121'}}>&nbsp;{isim}&nbsp;</li>
+                    <li><input autoComplete='off' onChange={(e) => setUserInput(e.target.value)} required id="home-isim-input" /><button disabled={!userInput} >Selamla</button></li>
+                    <li>{isim}&nbsp;</li>
                     <li>&nbsp;</li>
                 </ul>
                 </form>
