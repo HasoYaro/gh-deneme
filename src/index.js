9057817226f04379b3157690fb95956eabd97f86
HasoYaro <s42combi@hotmail.com>
2023-08-04 10:58:48 +0300

    a

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import Footer from './pages/components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>haso</h1>
   <React.StrictMode>
+    <h1>haso</h1>
     <Header />
       <HashRouter HashType="hashbang">
         <div className='Parent'>
